5360de6f13a7ba64a49b8ca1ec880f0a9baffdcb
ivan <ivanongjiaen@gmail.com>
2023-04-29 08:15:20 +0800

    run permute

--- permute.sh
+++ permute.sh
@@ -14,7 +14,7 @@ do
     do
         for scoring_model in "${base_models[@]}"
         do
-            python run.py --output_name cross --base_model_name "$base_model" --scoring_model_name "$scoring_model" --mask_filling_model_name "$mask_model" --n_perturbation_list "$n_perturbation_list" --n_samples "$n_samples" --batch_size "$batch_size" --pct_words_masked 0.3 --span_length 2 --skip_baselines 
+            python run.py --output_name permute --base_model_name "$base_model" --scoring_model_name "$scoring_model" --mask_filling_model_name "$mask_model" --n_perturbation_list "$n_perturbation_list" --n_samples "$n_samples" --batch_size "$batch_size" --pct_words_masked 0.3 --span_length 2 --skip_baselines 
         done
     done
 done
