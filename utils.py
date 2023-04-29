828cc326fb3d3646c7f620e9f9f81812346e8311
ivan <ivanongjiaen@gmail.com>
2023-04-30 00:32:28 +0800

    change utils.py

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def get_config_dicts(results_folder = "results/permute/"):
     config_folders = sorted(os.listdir(results_folder))
 
     itemss = [tuple([folder] + re.split('_', folder)) for folder in config_folders]
-    keys =  ("folder", "base_model", "scoring_model", "mask_model", "sampling")
+    keys =  ("folder", "base_model", "scoring_model", "mask_model", "dataset", "sampling")
 
     config_dicts = [{key:item for key, item in zip(keys, items)} for items in itemss]
 
@@ -32,8 +32,7 @@ def get_config_dicts(results_folder = "results/permute/"):
         # sort by date and return the latest z path
         return sorted(glob.glob(f"{results_folder}{folder_name}/**/{z_results_glob}"))[-1]
 
-    # TODO: replace dummy "dataset" key with real info
-    config_dicts = [{**config_dict, "dataset": "xsum", "d_path": get_d_path(config_dict["folder"]), "z_path": get_z_path(config_dict["folder"])} for config_dict in config_dicts]
+    config_dicts = [{**config_dict, "d_path": get_d_path(config_dict["folder"]), "z_path": get_z_path(config_dict["folder"])} for config_dict in config_dicts]
 
     return config_dicts
 
